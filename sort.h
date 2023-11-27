ae5ed494dd9d7e4c6da321a601f4429a684a912d
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-27 22:29:37 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -29,5 +29,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 void shell_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+
+
+int getMax(int *array, size_t s);
 #endif 
